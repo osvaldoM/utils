6c7ede81e8022cc31fdf792dd356dcbd827823fa
Osvaldo Maria <mekymaria@gmail.com>
2019-05-15 16:32:29 +0200

    Add mozambique provinces and districts from apimoz.herokuapp.com

---
+++ provincias.sql
@@ -0,0 +1,11 @@
+INSERT INTO provincias(id,nome) VALUES (3,'Maputo Cidade');
+INSERT INTO provincias(id,nome) VALUES (4,'Maputo Província');
+INSERT INTO provincias(id,nome) VALUES (5,'Gaza');
+INSERT INTO provincias(id,nome) VALUES (6,'Inhambane');
+INSERT INTO provincias(id,nome) VALUES (7,'Manica');
+INSERT INTO provincias(id,nome) VALUES (8,'Sofala');
+INSERT INTO provincias(id,nome) VALUES (9,'Zambézia');
+INSERT INTO provincias(id,nome) VALUES (10,'Tete');
+INSERT INTO provincias(id,nome) VALUES (11,'Nampula');
+INSERT INTO provincias(id,nome) VALUES (12,'Niassa');
+INSERT INTO provincias(id,nome) VALUES (13,'Cabo delgado');
